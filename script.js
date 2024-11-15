19b4e47a645466cbdbfb27f7ade2ca56a740f738
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-11-15 17:43:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function firstWord(s) {
+
+	  s = s.trim();
+
+    // Find the index of the first space
+    const spaceIndex = s.indexOf(' ');
+
+    // If no space is found, return the entire string
+    if (spaceIndex === -1) {
+        return s;
+    }
+
+    // Otherwise, return the substring before the first space
+    return s.slice(0, spaceIndex);
   // your code here
 }
 
